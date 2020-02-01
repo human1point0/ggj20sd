58ebf3f581060bccddc80c916b42b48625e409bf
douglas-rks <douglas@redkiltstudios.com>
2020-02-01 10:40:50 -0800

    add test scene

--- Assets/Scripts/GameAdmin.cs
+++ Assets/Scripts/GameAdmin.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 //admins the game state
 public class GameAdmin : MonoBehaviour
 {
-
+    public Transform ScrollWorldObject;
+    public float ScrollSpeed = 1;
     public enum GameState
     {
         StartMenu,
@@ -24,12 +25,31 @@ public class GameAdmin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _state = GameState.InGame;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (_state)
+        {
+            case GameState.InGame:
+                DoGameUpdate();
+                break;
+        }        
+    }
+
+
+    void DoGameUpdate()
+    {
+        if (ScrollWorldObject)
+        {
+            var speed = Time.deltaTime * ScrollSpeed;
+            ScrollWorldObject.position += Vector3.back * speed;
+        }
+        else
+        {
+            Debug.Log("Connect scroll object");
+        }
     }
 }
