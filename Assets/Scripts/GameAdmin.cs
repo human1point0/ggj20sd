05b263a93799d4d7de8b5ead36805679dcf99c87
onix <g.eduardodeluna@gmail.com>
2020-02-01 09:39:07 -0800

    Game and Input Admin Input Map switching

--- Assets/Scripts/GameAdmin.cs
+++ Assets/Scripts/GameAdmin.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 //admins the game state
 public class GameAdmin : MonoBehaviour
 {
-
+    public GameObject inputAdminContainer;
+    private InputAdmin _inputAdmin;
+   
     public enum GameState
     {
         StartMenu,
@@ -21,15 +24,28 @@ public class GameAdmin : MonoBehaviour
     {
         return _state;
     }
+
+    private void Awake()
+    {
+        _inputAdmin = inputAdminContainer.GetComponent(typeof(InputAdmin)) as InputAdmin;
+        _inputAdmin.setGameAdminReference(this);
+        _inputAdmin.setPlayerInputReference(inputAdminContainer.GetComponent(typeof(PlayerInput)) as PlayerInput);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        _inputAdmin.SetUIMode();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ping()
+    {
+        print("ping" + this.GetType().Name);
     }
 }
--- Assets/Scripts/InputAdmin.cs
+++ Assets/Scripts/InputAdmin.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputAdmin : MonoBehaviour
 {
     RepairActions controls = null;
+    GameAdmin _admin = null;
+    PlayerInput _playerInput = null;
 
     private void Awake()
     {
+        print("Awakened");
         controls = new RepairActions();
     }
 
@@ -31,5 +35,33 @@ public class InputAdmin : MonoBehaviour
         controls.Player.Disable();
     }
 
+    public void SetUIMode()
+    {
+        DisablePlayer();
+        EnableUI();
+        _playerInput.SwitchCurrentActionMap("UI");
+    }
+
+    public void SetGameplayMode()
+    {
+        DisableUI();
+        EnablePlayer();
+        _playerInput.SwitchCurrentActionMap("Player");
+    }
+
+    public void setGameAdminReference(GameAdmin admin)
+    {
+        _admin = admin;
+    }
+
+    public void setPlayerInputReference(PlayerInput playerInput)
+    {
+        _playerInput = playerInput;
+    }
+
+    public void ping()
+    {
+        print("ping" + this.GetType().Name);
+    }
     //Vector2 vec = controls.Player.MoveLeftCharacter.ReadValue<Vector2>();
 }
